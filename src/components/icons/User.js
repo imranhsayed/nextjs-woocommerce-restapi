a6d651133c336b09ee472aafc91f79a3d0232169
Imran <imranhsayed@gmail.com>
2021-08-29 14:03:40 +0530

    Add Footer info;

---
+++ src/components/icons/User.js
@@ -0,0 +1,19 @@
+import * as React from "react";
+
+function SvgUser(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      width={18}
+      height="auto"
+      stroke="currentColor"
+      {...props}
+    >
+      <path d="M16 7a4 4 0 11-8 0 4 4 0 018 0zm-4 7a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+    </svg>
+  );
+}
+
+export default SvgUser;
--- src/components/layouts/header/index.js
+++ src/components/layouts/header/index.js
@@ -1,12 +1,15 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import { useState } from 'react';
 import { isEmpty } from 'lodash';
 
-import {BurgerIcon, TailwindIcon} from '../../icons';
+import { BurgerIcon, TailwindIcon, Bag, User, Wishlist } from '../../icons';
 
 const Header = ( { header } ) => {
 	
 	const { headerMenuItems, siteDescription, siteLogoUrl, siteTitle, favicon } = header || {};
+	
+	const [ isMenuVisible, setMenuVisibility ] = useState( false );
 	
 	return (
 		<>
@@ -22,7 +25,8 @@ const Header = ( { header } ) => {
 								<a>
 									{
 										siteLogoUrl ? (
-											<img className="mr-2" src={ siteLogoUrl } alt={ `${ siteTitle } logo` } width="86"
+											<img className="mr-2" src={ siteLogoUrl } alt={ `${ siteTitle } logo` }
+											     width="86"
 											     height="86"/>
 										) : <TailwindIcon/>
 									}
@@ -37,47 +41,41 @@ const Header = ( { header } ) => {
 						</div>
 						<div className="block lg:hidden">
 							<button
+								onClick={() => setMenuVisibility( ! isMenuVisible )}
 								className="flex items-center px-3 py-2 border rounded text-black border-black hover:text-black hover:border-black">
-								<BurgerIcon/>
+								<BurgerIcon className="fill-current h-3 w-3"/>
 							</button>
 						</div>
-						<div
-							className="h-0 w-full overflow-hidden lg:h-full flex-grow lg:flex lg:items-center lg:w-auto">
+						<div className={`${ isMenuVisible ? 'max-h-full' : 'h-0' } overflow-hidden w-full lg:h-full block flex-grow lg:flex lg:items-center lg:w-auto`}>
 							<div className="text-sm font-medium uppercase lg:flex-grow">
 								{ ! isEmpty( headerMenuItems ) && headerMenuItems.length ? headerMenuItems.map( menuItem => (
-									<Link key={menuItem?.ID} href={ menuItem?.url || '/' }>
+									<Link key={ menuItem?.ID } href={ menuItem?.url || '/' }>
 										<a className="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-black mr-10"
-										   dangerouslySetInnerHTML={{__html: menuItem.title}}/>
+										   dangerouslySetInnerHTML={ { __html: menuItem.title } }/>
 									</Link>
 								) ) : null }
 							</div>
 							<div className="text-sm font-medium">
 								<a href="#responsive-header"
-								   className="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-black mr-10">
-									<svg xmlns="http://www.w3.org/2000/svg" className="hidden lg:block m-auto"
-									     fill="none" viewBox="0 0 24 24" width="18" height="auto" stroke="currentColor">
-										<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-										      d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path>
-									</svg>
+								   className="flex mt-4 lg:inline-block lg:mt-0 text-black hover:text-black mr-10">
+									<span className="flex flex-row items-center lg:flex-col">
+									<User className="mr-1 lg:mr-0"/>
 									Profile
+									</span>
 								</a>
 								<a href="#responsive-header"
-								   className="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-black mr-10">
-									<svg xmlns="http://www.w3.org/2000/svg" className="hidden lg:block m-auto"
-									     fill="none" viewBox="0 0 24 24" width="18" height="auto" stroke="currentColor">
-										<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-										      d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"></path>
-									</svg>
-									Wishlist
+								   className="flex mt-4 lg:inline-block lg:mt-0 text-black hover:text-black mr-10">
+									<span className="flex flex-row items-center lg:flex-col">
+										<Wishlist className="mr-1 lg:mr-0"/>
+										Wishlist
+									</span>
 								</a>
-								<a className="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-black mr-10"
+								<a className="flex mt-4 lg:inline-block lg:mt-0 text-black hover:text-black mr-10"
 								   href="/cart/">
-									<svg xmlns="http://www.w3.org/2000/svg" className="hidden lg:block m-auto"
-									     fill="none" viewBox="0 0 24 24" width="18" height="auto" stroke="currentColor">
-										<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-										      d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"></path>
-									</svg>
+									<span className="flex flex-row items-center lg:flex-col">
+									<Bag className="mr-1 lg:mr-0"/>
 									Bag
+									</span>
 								</a>
 							</div>
 						</div>
