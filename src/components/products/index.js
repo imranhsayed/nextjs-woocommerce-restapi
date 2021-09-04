a6f38eddfcdf35688bf7dffb0dd85be5d3614c02
Imran <imranhsayed@gmail.com>
2021-09-04 15:43:48 +0530

    Add nextjs Image to the product

--- src/components/products/index.js
+++ src/components/products/index.js
@@ -1,5 +1,6 @@
 import { isArray, isEmpty } from 'lodash';
 import Link from 'next/link';
+import Image from '../image';
 
 const Products = ({ products }) => {
 	
@@ -7,20 +8,28 @@ const Products = ({ products }) => {
 		return null;
 	}
 	
-	console.warn(products)
-	
 	return (
 		<div className="flex flex-wrap -mx-2 overflow-hidden">
 			
-			{ products.length ? products.map( product => (
-				<div key={ product?.id } className="my-2 px-2 w-full overflow-hidden sm:w-1/2 md:w-1/3 xl:w-1/4">
-					<Link href={product?.permalink ?? '/'}>
-						<a>
-							<h3>{ product?.name ?? '' }</h3>
-						</a>
-					</Link>
-				</div>
-			) ) : null }
+			{ products.length ? products.map( product => {
+				const img = product?.images?.[0] ?? {};
+				return (
+					<div key={ product?.id } className="my-2 px-2 w-full overflow-hidden sm:w-1/2 md:w-1/3 xl:w-1/4">
+						<Link href={product?.permalink ?? '/'}>
+							<a>
+								<Image
+									sourceUrl={ img?.src ?? '' }
+									altText={ img?.alt ?? ''}
+									title={ product?.name ?? '' }
+									width="380"
+									height="380"
+								/>
+								<h3 className="font-bold uppercase">{ product?.name ?? '' }</h3>
+							</a>
+						</Link>
+					</div>
+				)
+			} ) : null }
 		
 		</div>
 	)
