5c252c0331b10f870e1f7b15187d3ee86d567085
Imran Sayed <imranhsayed@gmail.com>
2023-02-19 16:31:36 +0530

    Replace backend with frontend url

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -11,11 +11,12 @@ import { AppProvider } from '../context';
 import Header from './header';
 import Footer from './footer';
 import Seo from '../seo';
-import { sanitize } from '../../utils/miscellaneous';
+import { replaceBackendWithFrontendUrl, sanitize } from '../../utils/miscellaneous';
 
 
 const Layout = ({children, headerFooter, seo, uri }) => {
 	const { header, footer } = headerFooter || {};
+	const yoastSchema = seo?.schema ? replaceBackendWithFrontendUrl( JSON.stringify( seo.schema ) ) : null;
 
 	return (
 		<AppProvider>
@@ -23,11 +24,11 @@ const Layout = ({children, headerFooter, seo, uri }) => {
 				<Seo seo={ seo || {} } uri={ uri || '' }/>
 				<Head>
 					{ header?.favicon ? <link rel="shortcut icon" href={ header.favicon }/> : null }
-					{ seo?.schema ? ( <script
+					{ yoastSchema ? ( <script
 							type="application/ld+json"
 							className="yoast-schema-graph"
 							key="yoastSchema"
-							dangerouslySetInnerHTML={ { __html: sanitize( JSON.stringify( seo.schema ) ) } }
+							dangerouslySetInnerHTML={ { __html: sanitize( yoastSchema ) } }
 						/> ) : null }
 				</Head>
 				<Header header={header}/>
--- src/utils/miscellaneous.js
+++ src/utils/miscellaneous.js
@@ -10,3 +10,22 @@ import DOMPurify from 'dompurify';
 export const sanitize = ( content ) => {
 	return 'undefined' !== typeof window ? DOMPurify.sanitize( content ) : content;
 };
+
+/**
+ * Replace backend url with front-end url.
+ *
+ * @param {String} data Data.
+ *
+ * @return formattedData Formatted data.
+ */
+export const replaceBackendWithFrontendUrl = ( data ) => {
+	if ( ! data || 'string' !== typeof data ) {
+		return '';
+	}
+	
+	// First replace all the backend-url with front-end url
+	let formattedData = data.replaceAll( process.env.NEXT_PUBLIC_WORDPRESS_SITE_URL, process.env.NEXT_PUBLIC_SITE_URL );
+	
+	// Replace only the upload urls for images back to back to back-end url, since images are hosted in the backend.
+	return formattedData.replaceAll( `${ process.env.NEXT_PUBLIC_SITE_URL }/wp-content/uploads`, `${ process.env.NEXT_PUBLIC_WORDPRESS_SITE_URL }/wp-content/uploads` );
+}
